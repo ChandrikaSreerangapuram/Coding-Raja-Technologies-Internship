28c252b3ead4138fbb3409f557799d332d328899
ChandrikaSreerangapuram <chandrikasreerangapuram@gmail.com>
2024-01-20 18:37:37 +0530

    Blog App

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,24 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Chandrika' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Chandrika's Blog! This blog-app was created by Chandrika Sreerangapuram
+              which is part of my internship project.
+            </p>
+            
+            <p>
+              I have done this project during my Intership period and my resources are youtube and my internship references.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
